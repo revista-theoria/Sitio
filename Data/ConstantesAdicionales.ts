a72c3858b1092cf30d2f8df31502461daa32e00d
victorlaureanovega <victorlaureanovega@gmail.com>
2026-01-14 16:41:28 -0600

    Visualizar sitio completo

--- Data/ConstantesAdicionales.ts
+++ Data/ConstantesAdicionales.ts
@@ -1,6 +1,6 @@
 // Detalles sobre la próxima presentación del número más reciente
 export const informacionPresentacion = "El evento se llevará a cabo el sábado 14 de junio a las 18:00, en el café Slava, ubicado en Justo Sierra #2040, colonia Ladrón de Guevara, Guadalajara, Jalisco. Habrá lecturas de los colaboradores, exhibición de obras y convivencia con los asistentes. ¡Te esperamos!";
-export const infoPresentacionVisible = false;
+export const infoPresentacionVisible = true;
 
 // Enlace del formulario de envío de propuestas para cuando hay una convocatoria activa
 export const formularioActivo = "https://docs.google.com/forms/d/e/1FAIpQLSc-J1w2F4mnPzjHS7wFM8aZi-Bv8D2kpGt3CA4aBt8VoKv6Cg/viewform?embedded=true";
--- Data/Numeros.ts
+++ Data/Numeros.ts
@@ -4,7 +4,7 @@ import type { Numero } from "./Interfaces";
 Mientras la convocatoria esté activa, no se muestra información relacionada a la presentación de ese número.
 Cuando se muestre esta información, la convocatoria ya habrá cerrado, por lo que se oculta de la página principal
 */
-export const convocatoriaActiva = false;
+export const convocatoriaActiva = true;
 
 // Arreglo que contiene la información relevante de los números de la revista
 export const numeros: Numero[] = [
